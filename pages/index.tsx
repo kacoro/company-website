f56e978d2e06819795ef7b7de54fcda3c9a6f886
kacoro <kacoro@qq.com>
2019-12-25 05:51:50 +0800

    add swipeable-viesw

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,15 @@ import Link from 'next/link'
 import Layout from '../components/Layout';
 import { NextPage } from 'next';
 import { PostLinkType } from '../interfaces'
+import SwipeableViews from 'react-swipeable-views';
+import { virtualize,autoPlay,bindKeyboard } from 'react-swipeable-views-utils';
+import { mod } from 'react-swipeable-views-core';
+import Pagination from '../components/Swipe/Pagination';
+
+const EnhancedSwipeableViews = bindKeyboard(autoPlay(virtualize(SwipeableViews)));
+
 import * as React from 'react'
+import { useState } from 'react';
 function getPosts() {
   return [
     { id: 'hello-nextjs', title: 'Hello Next.js' },
@@ -10,6 +18,25 @@ function getPosts() {
     { id: 'deploy-nextjs', title: 'Deploy apps with ZEIT' }
   ];
 }
+const styles = {
+  root: {
+    position: 'relative',
+  },
+  slide: {
+    padding: 15,
+    minHeight: 100,
+    color: '#fff',
+  },
+  slide1: {
+    backgroundColor: '#FEA900',
+  },
+  slide2: {
+    backgroundColor: '#B3DC4A',
+  },
+  slide3: {
+    backgroundColor: '#6AC0FF',
+  },
+};
 
 const PostLink: React.FunctionComponent<PostLinkType> = ({ id,title}) => (
   <li>  
@@ -34,20 +61,61 @@ const PostLink: React.FunctionComponent<PostLinkType> = ({ id,title}) => (
       `}</style>
   </li>
 );
+
+function slideRenderer(params: { index: any; key: any; }) {
+  const { index, key } = params;
+
+  switch (mod(index, 3)) {
+    case 0:
+      return (
+        <div key={key} >
+           <img className="img-fluid"  src={`/images/banner/banner1.png`} />
+        </div>
+      );
+
+    case 1:
+      return (
+        <div key={key} >
+         <img className="img-fluid"  src={`/images/banner/banner2.png`} />
+        </div>
+      );
+
+    case 2:
+      return (
+        <div key={key} >
+          <img className="img-fluid"  src={`/images/banner/banner3.png`} />
+        </div>
+      );
+
+    default:
+      return null;
+  }
+}
 const Index: NextPage<{ userAgent: string }> = ({ userAgent }) => {
+    const [index, setIndex] = useState(0)
+    function handleChangeIndex(index: number){
+        setIndex(mod(index,3))
+    }
+
     return (
       <Layout>
-      <img className="img-fluid"  src={`/images/banner/banner1.png`} />
+        <div className="swiper">
+      <EnhancedSwipeableViews  index={index} onChangeIndex={handleChangeIndex} enableMouseEvents slideRenderer={slideRenderer} />
+      <Pagination dots={3} index={index} onChangeIndex={handleChangeIndex} />
+      </div>
       <div className="container-xl">
       <ul>
         {getPosts().map(post => (
           <PostLink key={post.id} id={post.id} title={post.title} />
         ))}
       </ul>
+     
       </div>
       
       <style jsx>{`
-      
+        .swiper{
+          position:relative;
+        }
         ul {
           padding: 0;
         }
